b79381a42f7cd8264dc30ce1c4179766f6311535
Redomeire <redomeire2@gmail.com>
2025-05-10 19:00:51 +0700

    feat: add env builder

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
-
 export const environment = {
-  AUTH0_CLIENT_ID: 'y596qwIM7duYlETvChiviufvUHPOQZjB',
-  AUTH0_CLIENT_SECRET: 'HEHEmhlA8xfna2f_1ikC6BMB-6Htul9aZbKqAIavDF-Is2mIqIi8Gz9hOA2JY8xq',
-  AUTH0_CLIENT_DOMAIN: 'dev-u7xebu50d0ivpnsj.jp.auth0.com',
+  AUTH0_CLIENT_ID: import.meta.env.NG_APP_AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET: import.meta.env.NG_APP_AUTH0_CLIENT_SECRET,
+  AUTH0_CLIENT_DOMAIN: import.meta.env.NG_APP_AUTH0_CLIENT_DOMAIN,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  AUTH0_CLIENT_ID: process.env['AUTH0_CLIENT_ID'],
-  AUTH0_CLIENT_SECRET: process.env['AUTH0_CLIENT_SECRET'],
-  AUTH0_CLIENT_DOMAIN: process.env['AUTH0_CLIENT_DOMAIN'],
+  AUTH0_CLIENT_ID: import.meta.env.NG_APP_AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET: import.meta.env.NG_APP_AUTH0_CLIENT_SECRET,
+  AUTH0_CLIENT_DOMAIN: import.meta.env.NG_APP_AUTH0_CLIENT_DOMAIN,
 };
